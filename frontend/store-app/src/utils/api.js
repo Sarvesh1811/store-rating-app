9141cf3994c8b38207e84a4f1f8e10d3b7593590
Sarvesh1811 <pawarsarvesh1811@gmail.com>
2026-02-12 17:31:13 +0530

    Updated frontend API to production backend

--- frontend/store-app/src/utils/api.js
+++ frontend/store-app/src/utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:4000/api",
+  baseURL: "https://store-rating-app-zuu5.onrender.com",
 });
 
 API.interceptors.request.use((req) => {
